6bbd0bbcfa67d4f27b9e2aee1eaa374b57d252e7
rrobert <rutaburarobert@gmail.com>
2020-05-05 00:49:36 +0300

    Updated swagger file

--- api.R
+++ api.R
@@ -11,14 +11,14 @@ model <- readRDS("disease-prediction-model.rds")
 #* Return the predicted diseases
 
 #* @param diseases The diseases to predict
-#* @param located The location name
-#* @param month The month to predict
+#* @param user_located The location name
+#* @param user_month The month to predict
 
 #* @post /predict_disease_incident
-function(diseases,location, located, month) {
+function(diseases,user_located, user_month) {
   
-  liveData <- read.csv(url("https://ndownloader.figshare.com/files/22519493"))
-  df  <- liveData
+  originalData <- read.csv(url("https://ndownloader.figshare.com/files/22519493"))
+  df  <- originalData
   change2<- function(vec){
     tb <- c( "1" = "Nakasongola", "2" = "Entebbe","3" = "Butambala", "4" = "Gulu", "5" = "Kampala", "6" ="Sembabule", "7" = "Soroti", "8" = "Kitgum", "9"="Nakaseke")
     unname(tb[vec])
@@ -30,8 +30,8 @@ function(diseases,location, located, month) {
   drops <- c("loca")
   df<-df[ , !(names(df) %in% drops)]
   df<-df[,-16]
-  liveData <- df
-  liveData
+  originalData <- df
+  originalData #copy of original data with location names added
   #corelations
   x<-df[3:11]
   cor(x, use="complete.obs", method="pearson")## note mean temp, humidity and max temp
@@ -52,19 +52,15 @@ function(diseases,location, located, month) {
   sel$month2<-as.numeric(sel$month)
   ##clean up
   rm(x,y,dfs,say,df,change2)
-  
-  write.csv(sel, file= "Disease-Weather-demodb.csv",row.names = FALSE) #this saves the demopractice file of scaled data
-  sel <- read.csv("Disease-Weather-demodb.csv")
-  
-  model <- readRDS("disease-prediction-model.rds")
-  
+
   t1<-predict(model, newdata = sel, allow_new_levels=TRUE)
-  outcomeDF<-cbind(sel,t1)
+  results<-cbind(sel,t1)
   
-  ld <- liveData %>% select(preasure,rain,sun,humidity,mean_temp,max_temp,min_temp, wind_gust,mean_wind_spd, disease, located, month) %>% filter(disease %in% c("Typhoid", "Asthma")) %>% filter(located %in% c("Kitgum")) %>% filter(month %in% c("August"))
+  estimatesDF <- results %>% select(disease, located, month, Estimate) %>% filter(disease %in% c(diseases)) %>% filter(located %in% c(user_located)) %>% filter(month %in% c(user_month)) %>% select(Estimate)
+  response <- originalData %>% select(preasure,rain,sun,humidity,mean_temp,max_temp,min_temp, wind_gust,mean_wind_spd, disease, located, month) %>% filter(disease %in% c(diseases)) %>% filter(located %in% c(user_located)) %>% filter(month %in% c(user_month))
   
-  odf <- outcomeDF %>% select(disease, located, month, Estimate) %>% filter(disease %in% c("Typhoid", "Asthma")) %>% filter(located %in% c("Kitgum")) %>% filter(month %in% c("August")) %>% select(Estimate)
-  ld <- cbind(ld,odf)
-  ld
+  response <- cbind(response,estimatesDF)
+  
+  response
 }
 
